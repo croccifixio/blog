4f9b43c051520d0a6904b6aea6e7341910490804
Emmanuel <manueldno@gmail.com>
2016-07-10 08:30:31 +0000

    Reorganise website assets
    Font, image, CSS and JS files have been moved into the source/assets directory

--- config.rb
+++ config.rb
@@ -2,6 +2,12 @@
 # Page options, layouts, aliases and proxies
 ###
 
+# Use Assets directory for CSS, JS, font and image files
+set :css_dir, 'assets/stylesheets'
+set :font_dir, 'assets/fonts'
+set :images_dir, 'assets/images'
+set :js_dir, 'assets/javascripts'
+
 # Per-page layout changes:
 #
 # With no layout
@@ -26,10 +32,11 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  blog.permalink = "blog/{year}/{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
+  blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
@@ -47,9 +54,9 @@ end
 
 page "/feed.xml", layout: false
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
