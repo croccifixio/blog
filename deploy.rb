41636a25afa1bf9c993582250a23ca5764de7fcd
Croccifixio <aizenkrosis@hotmail.com>
2016-12-30 23:53:16 +0100

    Add deployment script coded in ruby

---
+++ deploy.rb
@@ -0,0 +1,18 @@
+# Get array of font files (.woff and .woff2)
+Dir.chdir "source/assets/fonts"
+font_list = Dir.glob("*.{woff,woff2}")
+
+# Mark font files as changed
+font_list.each do |font|
+  system "git update-index --no-assume-unchanged #{font}"
+end
+
+# Push to production
+system "git commit -am 'Push font files to server'"
+system "git push -f heroku master"
+
+# Revert changes locally
+font_list.each do |font|
+	system "git reset HEAD #{font}"
+	system "git update-index --assume-unchanged #{font}"
+end
