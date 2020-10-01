bf8b5db972b44b1d37b1185e00816ea5e28be05e
Croccifixio <aizenkrosis@hotmail.com>
2020-10-01 18:10:51 +0200

    chore: update location on about me page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,7 +3,7 @@
     .blog-post
       p
       | My name is Emmanuel Odongo.
-      | I am a frontend developer based in Łódź, Poland.
+      | I am a frontend developer based in Buġibba, Malta.
       p
       | I am currently dipping my toes into the world of functional programming and slowly building a collection of small mechanical keyboards.
 </template>
