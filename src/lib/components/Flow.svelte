20a271c7c243c8acad2531f5796f4409b71d3c71
croccifixio <git@croccifix.io>
2022-03-16 22:27:45 +0100

    fix: correctly space arrow in flow component

--- src/lib/components/Flow.svelte
+++ src/lib/components/Flow.svelte
@@ -3,8 +3,19 @@
 </script>
 
 {#each steps as step, index}
-	<span class="ui-flow">{@html step}</span>
-	{#if index + 1 < steps.length}
-		" "&rarr;" "
-	{/if}
+	<span class="flow">
+		<span class="name">{@html step}</span>
+		{#if index + 1 < steps.length}&rarr;{/if}
+	</span>
 {/each}
+
+<style lang="scss">
+	.name {
+		border: 2px solid var(--c-text);
+		border-radius: 4px;
+		padding: 0 4px 4px;
+	}
+	.flow {
+		display: inline;
+	}
+</style>
