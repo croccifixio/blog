231e46899507a9a85acfc931893ea4aaab1d7085
croccifixio <git@croccifix.io>
2022-03-20 14:33:14 +0100

    fix: prevent jarring rendering of Hr component

--- src/lib/components/Hr.svelte
+++ src/lib/components/Hr.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import { media } from '$lib/stores/media';
 
 	export let spacing: string = '0';
@@ -17,37 +18,41 @@
 </script>
 
 <div bind:clientWidth style:margin-block={spacing}>
-	<svg {height} width={clientWidth} xmlns="http://www.w3.org/2000/svg">
-		<defs>
-			<pattern fill="transparent" {height} id="pattern" width={width / clientWidth} x="0" y="0">
-				{#if pattern === 'triangular'}
-					<path
-						d={`M0 ${height * 0.2} l${width / 2} ${height * 0.6}`}
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-width="var(--border-width)"
-					/>
-					<path
-						d={`M${width / 2} ${height * 0.8} l${width / 2} -${height * 0.6}`}
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-width="var(--border-width)"
-					/>
-				{/if}
-				{#if pattern === 'sinusoidal'}
-					<path
-						d={`
-							M0 ${height * 0.475}
-							C${width * 0.2} ${height * 0.1} ${width * 0.33} ${height * 0.1} ${width * 0.5} ${height * 0.475}
-							C${width * 0.66} ${height * 0.9} ${width * 0.8} ${height * 0.9} ${width} ${height * 0.475}
-						`}
-						stroke="currentColor"
-						stroke-linecap="square"
-						stroke-width="var(--border-width)"
-					/>
-				{/if}
-			</pattern>
-		</defs>
-		<rect fill="url(#pattern)" {height} width={clientWidth} />
-	</svg>
+	{#if clientWidth}
+		<div transition:fade>
+			<svg {height} width={clientWidth} xmlns="http://www.w3.org/2000/svg">
+				<defs>
+					<pattern fill="transparent" {height} id="pattern" width={width / clientWidth} x="0" y="0">
+						{#if pattern === 'triangular'}
+							<path
+								d={`M0 ${height * 0.2} l${width / 2} ${height * 0.6}`}
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-width="var(--border-width)"
+							/>
+							<path
+								d={`M${width / 2} ${height * 0.8} l${width / 2} -${height * 0.6}`}
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-width="var(--border-width)"
+							/>
+						{/if}
+						{#if pattern === 'sinusoidal'}
+							<path
+								d={`
+								M0 ${height * 0.475}
+								C${width * 0.2} ${height * 0.1} ${width * 0.33} ${height * 0.1} ${width * 0.5} ${height * 0.475}
+								C${width * 0.66} ${height * 0.9} ${width * 0.8} ${height * 0.9} ${width} ${height * 0.475}
+							`}
+								stroke="currentColor"
+								stroke-linecap="square"
+								stroke-width="var(--border-width)"
+							/>
+						{/if}
+					</pattern>
+				</defs>
+				<rect fill="url(#pattern)" {height} width={clientWidth} />
+			</svg>
+		</div>
+	{/if}
 </div>
