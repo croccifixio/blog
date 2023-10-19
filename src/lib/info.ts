987f5cecca29c911d14d7ff95b82aef6c4625a51
croccifixio <git@croccifix.io>
2023-10-19 23:59:59 +0200

    chore: update site name and URL

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -1,6 +1,6 @@
 const SITE_DESCRIPTION =
 	'Emmanuel Odongo is a web developer who writes about web design and development.';
-const SITE_NAME = 'Croccifixio';
-const SITE_URL = 'https://croccifix.io';
+const SITE_NAME = 'Odongo';
+const SITE_URL = 'https://odongo.pl';
 
 export { SITE_DESCRIPTION, SITE_NAME, SITE_URL };
--- src/routes/meta/index.svelte
+++ src/routes/meta/index.svelte
@@ -1,7 +1,7 @@
 <details class="dropdown-list" open>
 	<summary><h2>Links</h2></summary>
 	<ul>
-		<li><a href="mailto:hello@croccifix.io">hello@croccifix.io</a> &ndash; Email.</li>
+		<li><a href="mailto:emmanuel@odongo.pl">emmanuel@odongo.pl</a> &ndash; Email.</li>
 		<li>
 			<a href="https://sr.ht/~croccifixio/">sr.ht/~croccifixio</a> &ndash; Source code repository host.
 		</li>
@@ -23,19 +23,19 @@
 	<summary><h2>Site</h2></summary>
 	<ul>
 		<li>
-			<a href="https://v1.croccifix.io/">v1.croccifix.io</a> &ndash; Built with
+			<a href="https://v1.odongo.pl/">v1.odongo.pl</a> &ndash; Built with
 			<a href="https://middlemanapp.com/">Middleman</a>, a Ruby-based static site generator.
 		</li>
 		<li>
-			<a href="https://v2.croccifix.io/">v2.croccifix.io</a> &ndash; Built with
+			<a href="https://v2.odongo.pl/">v2.odongo.pl</a> &ndash; Built with
 			<a href="https://gridsome.org/">Gridsome</a>, a Vue-based static site generator.
 		</li>
 		<li>
-			<a href="https://v3.croccifix.io/">v3.croccifix.io</a> &ndash; Built with
+			<a href="https://v3.odongo.pl/">v3.odongo.pl</a> &ndash; Built with
 			<a href="https://www.getzola.org/">Zola</a>, a Rust-based static site generator.
 		</li>
 		<li>
-			<a href="https://croccifix.io/">croccifix.io</a> &ndash; Built with
+			<a href="https://odongo.pl/">odongo.pl</a> &ndash; Built with
 			<a href="https://kit.svelte.dev/">Sveltekit</a>, a Svelte-based web app framework.
 		</li>
 	</ul>
@@ -45,9 +45,9 @@
 	<summary><h2>Thesis</h2></summary>
 	<ul>
 		<li>
-			<a href="https://thesis.croccifix.io/thesis.pdf">Thesis (PDF, 5.8MB)</a> &ndash; Undefended
+			<a href="https://thesis.odongo.pl/thesis.pdf">Thesis (PDF, 5.8MB)</a> &ndash; Undefended
 			thesis on the Design and Simulation of Periodic Ring Resonator Structures. Includes a
-			<a href="https://thesis.croccifix.io/">web app for editing the structures</a>.
+			<a href="https://thesis.odongo.pl/">web app for editing the structures</a>.
 		</li>
 	</ul>
 </details>
